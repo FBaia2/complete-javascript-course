2e4a8bac98917be1be9791df3c13baf50a11bfdb
FBaia2 <squadbaia@gmail.com>
2024-03-31 19:16:17 +0100

    why so ez

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,24 @@
+// let js = "amazing";
+// console.log(40 + 30);
+// console.log(23)
+// let firstName = "bob";
+// console.log(firstName)
+// firstName = "james";
+// console.log(firstName)
+// console.log(firstName)
+// your mom is gay
+
+// true;
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+// console.log(typeof javaScriptIsFun);
+// console.log(typeof "javaScriptIsFun");
+// console.log(typeof 1);
+// javaScriptIsFun = 1;
+
+// console.log(javaScriptIsFun);
+let age;
+age = 30;
+age = 31;
+const firstName = "Adam";
+firstName = 30;
