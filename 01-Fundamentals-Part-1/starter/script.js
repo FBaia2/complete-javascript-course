f196c12c8689362f0867f32fb2eaccaa9906a064
FBaia2 <squadbaia@gmail.com>
2024-03-31 21:24:47 +0100

    why so ez

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -17,8 +17,65 @@
 // javaScriptIsFun = 1;
 
 // console.log(javaScriptIsFun);
-let age;
-age = 30;
-age = 31;
-const firstName = "Adam";
-firstName = 30;
+// let age;
+// age = 30;
+// age = 31;
+// const firstName = "Adam";
+// firstName = 30;
+// let now = 10;
+// let then = 20
+// const ageJonas = now + then;
+// console.log(ageJonas);
+// now = 1;
+// then = 1
+// console.log(ageJonas);
+
+// let x = 10 + 5;
+// console.log(x)
+// x ++;
+// console.log(x)
+
+const markHeight = 169;
+const markWeight = 78;
+const johnHeight = 195;
+const johnWeight = 92;
+
+bmiJohn = johnWeight / (johnHeight ** 2);
+bmiMark = markWeight / (markHeight ** 2);
+
+const markHigherBMI = bmiMark > bmiJohn;
+
+console.log(markHigherBMI)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
